aeb10f830a9309337451a77303ffd8527fac91a6
Diovane Alves <alvdiovane@gmail.com>
2023-04-26 21:34:11 -0300

    incoming database and returned products

---
+++ src/models/purchaseOrder.ts
@@ -0,0 +1,24 @@
+import { model, Schema } from "mongoose";
+
+const purchaseOrderSchema = new Schema({
+  responsible: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    enem: ["Entrada", "Devolução"],
+    default: "Entrada",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  productId: { type: Schema.Types.ObjectId, ref: "Product", required: true },
+});
+
+export const purchaseOrder = model("purchaseOrder", purchaseOrderSchema);
